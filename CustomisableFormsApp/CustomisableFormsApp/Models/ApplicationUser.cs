54ade2effeb7611f095e20157ca8630b56f9443c
Jalal Uddin <jalal14uddin@gmail.com>
2024-10-29 17:18:39 +0600

    course project upload fourth time

---
+++ CustomisableFormsApp/CustomisableFormsApp/Models/ApplicationUser.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CustomisableFormsApp.Models
+{
+    public class ApplicationUser: IdentityUser
+    {
+        [Required]
+        public string Name { get; set; }
+        public string? StreetAddress { get; set; }
+        public DateTime RegistrationTime { get; set; } = DateTime.Now;
+
+        [PersonalData]
+        [Column(TypeName = "DateTime")]
+        public DateTime? LastLoginTime { get; set; }
+
+        [PersonalData]
+        [Column(TypeName = "nvarchar(50)")]
+        public string Status { get; set; } = "Active";
+        public List<string> Roles { get; set; } = new List<string>();
+    }
+}
---
+++ CustomisableFormsApp/CustomisableFormsApp/Utility/EmailSender.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace CustomisableFormsApp.Utility
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            //logic for sending email
+            return Task.CompletedTask;
+        }
+    }
+}
